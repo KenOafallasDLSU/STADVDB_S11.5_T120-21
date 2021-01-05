44fb89620c41a13ee3c52ccda13134de54703cb4
KenOafallasDLSU <kenneth_oafallas@dlsu.edu.ph>
2021-01-05 17:56:12 +0800

    Query done, no style

--- controllers/query41Controller.js
+++ controllers/query41Controller.js
@@ -1,10 +1,54 @@
 const query41Model = require('../models/query41Model.js')
 
 exports.renderMain = (req, res) => {
-  res.render('query41', {
-    pageCSS: "query41CSS",
-    pageJS: "query41JS",
-    pageTitle: "Query 4-1",
-    navbarTitle: "Query 4-1"
+  let promise = new Promise((resolve) => {
+    query41Model.queryDistrictNames((result) => {
+      resolve(result)
+    })
+  })
+  .then((districtList) => {
+    let cardTypeList = [
+      {
+        type: 'Gold',
+        val: 'gold'
+      },
+      {
+        type: 'Classic',
+        val: 'classic'
+      },
+      {
+        type: 'Junior',
+        val: 'junior'
+      }
+    ]
+
+    res.render('query41', {
+      pageCSS: "query41CSS",
+      pageJS: "query41JS",
+      pageTitle: "Query 4-1",
+      navbarTitle: "Query 4-1",
+      cardTypeList: cardTypeList,
+      districtList: JSON.parse(JSON.stringify(districtList)),
+      results: [
+        {
+          district: 'Manila',
+          type: 'Electric',
+          trans: 'PRIJEM',
+          sum: '100',
+          count: '1001'
+        }
+      ]
+    })
+  })
+}
+
+exports.postQuery41 = (req, res) => {
+  let cardType = req.body.cardType;
+  let district = req.body.district;
+  let transType = req.body.transType;
+
+  query41Model.query41(cardType, district, transType, (result) => {
+    console.log(result)
+    res.send(result)
   })
 }
--- models/query41Model.js
+++ models/query41Model.js
@@ -1,1 +1,31 @@
 const mysql = require('../mysql.js')
+
+exports.query41 = (cardType, district, transType, next) => {
+  console.log("in query 41")
+  let sql = 
+   `SELECT s.a2 as district, c.type as type, t.type as trans, SUM(t.amount) as sum, COUNT(card_id) as count
+    FROM (SELECT * FROM card WHERE type = "${cardType}") c JOIN disp d ON c.disp_id = d.disp_id
+          JOIN account a ON d.account_id = a.account_id
+          JOIN (SELECT account_id, amount, type FROM trans WHERE type = "${transType}") t ON a.account_id = t.account_id
+          JOIN (SELECT district_id, a2 FROM district WHERE a2 = "${district}") s ON a.district_id = s.district_id`
+  
+  console.log(sql)
+
+  mysql.db.query(sql, (err, result) => {
+    if (err) throw err
+    next(result)
+  })
+}
+
+/**
+ * helper to query district names for the dropdown menu
+ * @param {*} next 
+ */
+exports.queryDistrictNames = (next) => {
+  let sql = 'SELECT A2 FROM district ORDER BY A2 ASC'
+
+  mysql.db.query(sql, (err, result) => {
+    if (err) throw err
+    next(result)
+  })
+}
